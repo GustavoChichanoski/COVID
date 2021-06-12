b2ebeb9473662acd3ca0fc43d72e72ca1bd71071
Gustavo Chichanoski <gustavochichanoski0@gmail.com>
2021-06-11 22:26:33 -0300

    Adicionou gerador a segmentacao

--- src/dataset/generator_seg.py
+++ src/dataset/generator_seg.py
@@ -56,10 +56,10 @@ class SegmentationDataGenerator(Sequence):
         idf = (idx + 1) * self.batch_size
         batch_x, batch_y = self.x[idi:idf], self.y[idi:idf]
 
-        print(len(batch_x))
-
         batch_x = self.read_step(batch_x)
         batch_y = self.read_step(batch_y)
+
+        print(len(batch_x))
 
         batch_x = np.reshape(batch_x, shape)
         batch_y = np.reshape(batch_y, shape)
