204e51d1a653708b067e302e5a3a3fda3d8ba7be
Gustavo Chichanoski <gustavochichanoski0@gmail.com>
2021-06-25 10:05:01 -0300

    [BUG] Correcao Loss

--- seg_lung.py
+++ seg_lung.py
@@ -6,7 +6,7 @@ from src.models.segmentacao.segmentacao_model import Unet
 
 DIM = 128
 
-model = Unet(dim=DIM,rate=0.0,activation='elu', final_activation='sigmoid')
+model = Unet(dim=DIM,rate=0.25,activation='relu', final_activation='sigmoid')
 model.compile(loss='log_cosh_dice', rf=1)
 model.build()
 data_path = Path('D:\\Mestrado\\new_data')
@@ -14,18 +14,18 @@ dataset = SegmentationDataset(
     path_lung=data_path / 'lungs',
     path_mask=data_path / 'masks'
 )
-train, val = dataset.partition(val_size=0.2, tamanho=10)
+train, val = dataset.partition(val_size=0.2, tamanho=40)
 
 train_generator = SegDataGen(train[0], train[1], batch_size=1, dim=DIM)
 val_generator = SegDataGen(val[0], val[1], batch_size=1, dim=DIM)
+
+model.summary()
 
 model.fit(
     x=train_generator,
     validation_data=val_generator,
     epochs=2
 )
-
-model.summary()
 
 # optimizer = Adam(learning_rate=1e-3)
 # loss = DiceError()
