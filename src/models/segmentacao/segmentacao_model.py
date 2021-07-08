66983403e9e496dc9b5e5c64d58b3aca9c7b66c8
Gustavo Chichanoski <gustavochichanoski0@gmail.com>
2021-07-08 12:38:39 -0300

    [UPDATE] Change metric model checkpoint

--- src/models/segmentacao/segmentacao_model.py
+++ src/models/segmentacao/segmentacao_model.py
@@ -131,8 +131,8 @@ class Unet(Model):
     def inner_callbacks(self) -> List[Callback]:
         if self._lazy_callbacks is None:
             checkpoint = ModelCheckpoint(
-                '.\\best.weights.hdf5', monitor='val_loss',
-                verbose=1,save_best_only=True, mode='min',
+                '.\\best.weights.hdf5', monitor='val_f1',
+                verbose=1,save_best_only=True, mode='max',
                 save_weights_only=True,
             )
             # Metrica para a redução do valor de LR
